9d995740a02c0b067bfe84082ffce80a766f1c32
Colin Luo <mail@luozhihua.com>
2020-06-13 12:17:10 +0800

    兼容 iOS 13.4.1以及其他支持图片自动旋转的浏览器.

--- src/lrz.js
+++ src/lrz.js
@@ -1,14 +1,14 @@
 // 保证按需加载的文件路径正确
 __webpack_public_path__ = getJsDir('lrz') + '/';
-window.URL              = window.URL || window.webkitURL;
+window.URL = window.URL || window.webkitURL;
 
-var Promise          = require('Promise'),
+var Promise = require('Promise'),
     BlobFormDataShim = require('Blob.FormData.shim'),
-    exif             = require('exif');
+    exif = require('exif');
 
 
 var UA = (function (userAgent) {
-    var ISOldIOS     = /OS (\d)_.* like Mac OS X/g.exec(userAgent),
+    var ISOldIOS = /OS (\d)_.* like Mac OS X/g.exec(userAgent),
         isOldAndroid = /Android (\d.*?);/g.exec(userAgent) || /Android\/(\d.*?) /g.exec(userAgent);
 
     // 判断设备是否是IOS7以下
@@ -17,16 +17,16 @@ var UA = (function (userAgent) {
     // 判断是否android
     // 判断是否QQ浏览器
     return {
-        oldIOS    : ISOldIOS ? +ISOldIOS.pop() < 8 : false,
+        oldIOS: ISOldIOS ? +ISOldIOS.pop() < 8 : false,
         oldAndroid: isOldAndroid ? +isOldAndroid.pop().substr(0, 3) < 4.5 : false,
-        iOS       : /\(i[^;]+;( U;)? CPU.+Mac OS X/.test(userAgent),
-        android   : /Android/g.test(userAgent),
+        iOS: /\(i[^;]+;( U;)? CPU.+Mac OS X/.test(userAgent),
+        android: /Android/g.test(userAgent),
         mQQBrowser: /MQQBrowser/g.test(userAgent)
     }
 })(navigator.userAgent);
 
 
-function Lrz (file, opts) {
+function Lrz(file, opts) {
     var that = this;
 
     if (!file) throw new Error('没有收到图片，可能的解决方案：https://github.com/think2011/localResizeIMG/issues/7');
@@ -34,10 +34,10 @@ function Lrz (file, opts) {
     opts = opts || {};
 
     that.defaults = {
-        width    : null,
-        height   : null,
+        width: null,
+        height: null,
         fieldName: 'file',
-        quality  : 0.7
+        quality: 0.7
     };
 
     that.file = file;
@@ -51,16 +51,16 @@ function Lrz (file, opts) {
 }
 
 Lrz.prototype.init = function () {
-    var that         = this,
-        file         = that.file,
+    var that = this,
+        file = that.file,
         fileIsString = typeof file === 'string',
         fileIsBase64 = /^data:/.test(file),
-        img          = new Image(),
-        canvas       = document.createElement('canvas'),
-        blob         = fileIsString ? file : URL.createObjectURL(file);
+        img = new Image(),
+        canvas = document.createElement('canvas'),
+        blob = fileIsString ? file : URL.createObjectURL(file);
 
-    that.img    = img;
-    that.blob   = blob;
+    that.img = img;
+    that.blob = blob;
     that.canvas = canvas;
 
     if (fileIsString) {
@@ -97,21 +97,21 @@ Lrz.prototype.init = function () {
                     // 压缩文件太大就采用源文件,且使用原生的FormData() @source #55
                     if (typeof that.file === 'object' && base64.length > that.file.size) {
                         formData = new FormData();
-                        file     = that.file;
+                        file = that.file;
                     } else {
                         formData = new BlobFormDataShim.FormData();
-                        file     = dataURItoBlob(base64);
+                        file = dataURItoBlob(base64);
                     }
 
                     formData.append(that.defaults.fieldName, file, (that.fileName.replace(/\..+/g, '.jpg')));
 
                     resolve({
-                        formData : formData,
-                        fileLen : +file.size,
-                        base64  : base64,
+                        formData: formData,
+                        fileLen: +file.size,
+                        base64: base64,
                         base64Len: base64.length,
-                        origin   : that.file,
-                        file   : file
+                        origin: that.file,
+                        file: file
                     });
 
                     // 释放内存
@@ -131,35 +131,97 @@ Lrz.prototype.init = function () {
     });
 };
 
+/**
+ * 检测浏览器是否支持自动旋转图片
+ *
+ * 兼容 iOS 13.4.1 以及支持图片自动旋转的浏览器；
+ * 参照：https://juejin.im/post/5ec26d3c6fb9a043681f83c2
+ * */
+var testAutoOrientationImageURL =
+    'data:image/jpeg;base64,/9j/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAYAAAA' +
+    'AAAD/2wCEAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBA' +
+    'QEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQE' +
+    'BAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAf/AABEIAAEAAgMBEQACEQEDEQH/x' +
+    'ABKAAEAAAAAAAAAAAAAAAAAAAALEAEAAAAAAAAAAAAAAAAAAAAAAQEAAAAAAAAAAAAAAAA' +
+    'AAAAAEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwA/8H//2Q==';
+var isImageAutomaticRotation;
+Lrz.detectImageAutomaticRotation = function () {
+    return new Promise(function (resolve) {
+        if (isImageAutomaticRotation === undefined) {
+            var img = new Image();
+
+            img.onload = function () {
+                // 如果图片变成 1x2，说明浏览器对图片进行了回正
+                isImageAutomaticRotation = img.width === 1 && img.height === 2;
+
+                resolve(isImageAutomaticRotation);
+            };
+
+            img.src = testAutoOrientationImageURL;
+        } else {
+            resolve(isImageAutomaticRotation);
+        }
+    });
+}
+
 Lrz.prototype._getBase64 = function () {
-    var that   = this,
-        img    = that.img,
-        file   = that.file,
+    var that = this,
+        img = that.img,
+        file = that.file,
         canvas = that.canvas;
 
     return new Promise(function (resolve) {
         try {
             // 传入blob在android4.3以下有bug
             exif.getData(typeof file === 'object' ? file : img, function () {
-                that.orientation = exif.getTag(this, "Orientation");
+                var img = this;
+                Lrz.detectImageAutomaticRotation().then(function (supported) {
+                    // 兼容 iOS 13.4.1 以及支持图片自动旋转的浏览器；
+                    // 参照：https://juejin.im/post/5ec26d3c6fb9a043681f83c2
+                    if (!supported) {
+                        that.orientation = exif.getTag(img, "Orientation");
+                    }
 
-                that.resize = that._getResize();
-                that.ctx    = canvas.getContext('2d');
+                    that.resize = that._getResize();
+                    that.ctx = canvas.getContext('2d');
 
-                canvas.width  = that.resize.width;
-                canvas.height = that.resize.height;
+                    canvas.width = that.resize.width;
+                    canvas.height = that.resize.height;
 
-                // 设置为白色背景，jpg是不支持透明的，所以会被默认为canvas默认的黑色背景。
-                that.ctx.fillStyle = '#fff';
-                that.ctx.fillRect(0, 0, canvas.width, canvas.height);
+                    // 设置为白色背景，jpg是不支持透明的，所以会被默认为canvas默认的黑色背景。
+                    that.ctx.fillStyle = '#fff';
+                    that.ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-                // 根据设备对应处理方式
-                if (UA.oldIOS) {
-                    that._createBase64ForOldIOS().then(resolve);
-                }
-                else {
-                    that._createBase64().then(resolve);
-                }
+                    // 根据设备对应处理方式
+                    if (UA.oldIOS) {
+                        that._createBase64ForOldIOS().then(resolve);
+                    }
+                    else {
+                        that._createBase64().then(resolve);
+                    }
+                })
+
+                // // ============================================
+
+                // console.log(that.orientation, exif.getTag(this, "Orientation"))
+                // that.orientation = exif.getTag(this, "Orientation");
+                // that.resize = that._getResize();
+                // that.ctx = canvas.getContext('2d');
+
+                // canvas.width = that.resize.width;
+                // canvas.height = that.resize.height;
+
+                // // 设置为白色背景，jpg是不支持透明的，所以会被默认为canvas默认的黑色背景。
+                // that.ctx.fillStyle = '#fff';
+                // that.ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+                // // 根据设备对应处理方式
+                // if (UA.oldIOS) {
+                //     that._createBase64ForOldIOS().then(resolve);
+                // }
+                // else {
+                //     that._createBase64().then(resolve);
+                // }
             });
         } catch (err) {
             // 这样能解决低内存设备闪退的问题吗？
@@ -170,10 +232,10 @@ Lrz.prototype._getBase64 = function () {
 
 
 Lrz.prototype._createBase64ForOldIOS = function () {
-    var that        = this,
-        img         = that.img,
-        canvas      = that.canvas,
-        defaults    = that.defaults,
+    var that = this,
+        img = that.img,
+        canvas = that.canvas,
+        defaults = that.defaults,
         orientation = that.orientation;
 
     return new Promise(function (resolve) {
@@ -182,14 +244,14 @@ Lrz.prototype._createBase64ForOldIOS = function () {
 
             if ("5678".indexOf(orientation) > -1) {
                 mpImg.render(canvas, {
-                    width      : canvas.height,
-                    height     : canvas.width,
+                    width: canvas.height,
+                    height: canvas.width,
                     orientation: orientation
                 });
             } else {
                 mpImg.render(canvas, {
-                    width      : canvas.width,
-                    height     : canvas.height,
+                    width: canvas.width,
+                    height: canvas.height,
                     orientation: orientation
                 });
             }
@@ -200,12 +262,12 @@ Lrz.prototype._createBase64ForOldIOS = function () {
 };
 
 Lrz.prototype._createBase64 = function () {
-    var that        = this,
-        resize      = that.resize,
-        img         = that.img,
-        canvas      = that.canvas,
-        ctx         = that.ctx,
-        defaults    = that.defaults,
+    var that = this,
+        resize = that.resize,
+        img = that.img,
+        canvas = that.canvas,
+        ctx = that.ctx,
+        defaults = that.defaults,
         orientation = that.orientation;
 
     // 调整为正确方向
@@ -255,7 +317,7 @@ Lrz.prototype._createBase64 = function () {
         if (UA.oldAndroid || UA.mQQBrowser || !navigator.userAgent) {
             require(['jpeg_encoder_basic'], function (JPEGEncoder) {
                 var encoder = new JPEGEncoder(),
-                    img     = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    img = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
                 resolve(encoder.encode(img, defaults.quality * 100));
             })
@@ -267,20 +329,20 @@ Lrz.prototype._createBase64 = function () {
 };
 
 Lrz.prototype._getResize = function () {
-    var that        = this,
-        img         = that.img,
-        defaults    = that.defaults,
-        width       = defaults.width,
-        height      = defaults.height,
+    var that = this,
+        img = that.img,
+        defaults = that.defaults,
+        width = defaults.width,
+        height = defaults.height,
         orientation = that.orientation;
 
     var ret = {
-        width : img.width,
+        width: img.width,
         height: img.height
     };
 
     if ("5678".indexOf(orientation) > -1) {
-        ret.width  = img.height;
+        ret.width = img.height;
         ret.height = img.width;
     }
 
@@ -294,25 +356,25 @@ Lrz.prototype._getResize = function () {
     if (width && height) {
         if (scale >= width / height) {
             if (ret.width > width) {
-                ret.width  = width;
+                ret.width = width;
                 ret.height = Math.ceil(width / scale);
             }
         } else {
             if (ret.height > height) {
                 ret.height = height;
-                ret.width  = Math.ceil(height * scale);
+                ret.width = Math.ceil(height * scale);
             }
         }
     }
     else if (width) {
         if (width < ret.width) {
-            ret.width  = width;
+            ret.width = width;
             ret.height = Math.ceil(width / scale);
         }
     }
     else if (height) {
         if (height < ret.height) {
-            ret.width  = Math.ceil(height * scale);
+            ret.width = Math.ceil(height * scale);
             ret.height = height;
         }
     }
@@ -330,7 +392,7 @@ Lrz.prototype._getResize = function () {
  * 获取当前js文件所在路径，必须得在代码顶部执行此函数
  * @returns {string}
  */
-function getJsDir (src) {
+function getJsDir(src) {
     var script = null;
 
     if (src) {
@@ -354,7 +416,7 @@ function getJsDir (src) {
  *
  * @source http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
  */
-function dataURItoBlob (dataURI) {
+function dataURItoBlob(dataURI) {
     // convert base64/URLEncoded data component to raw binary data held in a string
     var byteString;
     if (dataURI.split(',')[0].indexOf('base64') >= 0)
@@ -371,7 +433,7 @@ function dataURItoBlob (dataURI) {
         ia[i] = byteString.charCodeAt(i);
     }
 
-    return new BlobFormDataShim.Blob([ia.buffer], {type: mimeString});
+    return new BlobFormDataShim.Blob([ia.buffer], { type: mimeString });
 }
 
 window.lrz = function (file, opts) {
